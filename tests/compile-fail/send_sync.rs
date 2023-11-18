02872dea5a143e2674b5ed661f7a8c2a9ba13bf8
Cody P Schafer <dev@codyps.com>
2023-11-17 21:33:33 -0500

    make trybuild work regardless of rust source code location

--- tests/compile-fail/send_sync.rs
+++ tests/compile-fail/send_sync.rs
@@ -4,15 +4,21 @@
 use std::cell::RefCell;
 use std::thread;
 
+// NOTE: we include this so the error points to local source which gives reliable trybuild output
+fn spawn<F>(f: F)
+where
+    F: FnOnce() + Send + 'static,
+{
+    thread::spawn(f).join().unwrap();
+}
+
 fn main() {
     let (w, mut r1) = local_rcu::slot(RefCell::new(0));
 
-    let t1 = thread::spawn(move || {
+    let t1 = spawn(move || {
         *r1.read().borrow_mut() = 1;
     });
     //~^ ERROR: `RefCell<i32>` cannot be shared between threads safely
-
-    t1.join().unwrap();
 
     assert_eq!(*w.read().borrow(), 0);
 }
