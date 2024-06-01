d0bd5829394a33fdcee45a10b551a3fb599ad8f6
Cody P Schafer <dev@codyps.com>
2024-06-01 01:13:33 -0400

    tests/a: name threads

--- tests/a.rs
+++ tests/a.rs
@@ -1,28 +1,36 @@
+use std::thread;
+
 #[test]
 fn send_100_from_1_to_1() {
     let n = 100usize;
     let (mut tx, mut rx) = local_rcu::slot(0usize);
 
-    let tx = std::thread::spawn(move || {
-        for i in 1..=n {
-            tx.write(Box::new(i));
-            std::thread::yield_now();
-        }
-    });
+    let tx = thread::Builder::new()
+        .name("producer".to_owned())
+        .spawn(move || {
+            for i in 1..=n {
+                tx.write(Box::new(i));
+                std::thread::yield_now();
+            }
+        })
+        .unwrap();
 
-    let rx = std::thread::spawn(move || {
-        let mut prev = 0;
-        loop {
-            let i = *rx.read();
-            if prev > i {
-                panic!("{} > {}", prev, i);
+    let rx = thread::Builder::new()
+        .name("consumer".to_owned())
+        .spawn(move || {
+            let mut prev = 0;
+            loop {
+                let i = *rx.read();
+                if prev > i {
+                    panic!("{} > {}", prev, i);
+                }
+                if i == n {
+                    break;
+                }
+                prev = i;
             }
-            if i == n {
-                break;
-            }
-            prev = i;
-        }
-    });
+        })
+        .unwrap();
 
     tx.join().unwrap();
     rx.join().unwrap();
@@ -35,29 +43,37 @@ fn send_100_from_1_to_m() {
     let (mut tx, rx) = local_rcu::slot(0usize);
 
     let mut rx_t = Vec::with_capacity(m);
-    for _ in 0..m {
+    for i in 0..m {
         let mut rx = rx.clone();
-        rx_t.push(std::thread::spawn(move || {
-            let mut prev = 0;
-            loop {
-                let i = *rx.read();
-                if prev > i {
-                    panic!("{} > {}", prev, i);
-                }
-                if i == n {
-                    break;
-                }
-                prev = i;
-            }
-        }));
+        rx_t.push(
+            thread::Builder::new()
+                .name(format!("consumer {i} of {m}"))
+                .spawn(move || {
+                    let mut prev = 0;
+                    loop {
+                        let i = *rx.read();
+                        if prev > i {
+                            panic!("{} > {}", prev, i);
+                        }
+                        if i == n {
+                            break;
+                        }
+                        prev = i;
+                    }
+                })
+                .unwrap(),
+        );
     }
 
-    let tx = std::thread::spawn(move || {
-        for i in 1..=n {
-            tx.write(Box::new(i));
-            std::thread::yield_now();
-        }
-    });
+    let tx = thread::Builder::new()
+        .name("producer".to_owned())
+        .spawn(move || {
+            for i in 1..=n {
+                tx.write(Box::new(i));
+                std::thread::yield_now();
+            }
+        })
+        .unwrap();
 
     tx.join().unwrap();
     for rx in rx_t {
